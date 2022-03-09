5fc030907d927a2134ce882b43c46cccfdf0487b
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2022-03-09 18:02:04 -0300

    Updated Source

---
+++ src/main/java/dev/_2lstudios/milkshakebungee/MilkshakeBungee.java
@@ -0,0 +1,10 @@
+package dev._2lstudios.milkshakebungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class MilkshakeBungee extends Plugin {
+    @Override
+    public void onEnable() {
+        // Do nothing
+    }
+}
